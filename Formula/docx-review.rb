d8325ebf4141e2f026606c7ec06810d94382f35e
henry <henrybloomingdale@macmini-2.local>
2026-02-14 15:18:32 -0500

    Update docx-review to 1.1.0 (add --read mode)

--- Formula/docx-review.rb
+++ Formula/docx-review.rb
@@ -2,25 +2,25 @@ class DocxReview < Formula
   desc "Add tracked changes and comments to Word documents from the CLI"
   homepage "https://github.com/henrybloomingdale/docx-review"
   license "MIT"
-  version "1.0.0"
+  version "1.1.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/henrybloomingdale/docx-review/releases/download/v1.0.0/docx-review-darwin-arm64"
-      sha256 "36359b6e0499a387d7fc227abb66e6188fb2206f5655b3caac75e2b0b573b099"
+      url "https://github.com/henrybloomingdale/docx-review/releases/download/v1.1.0/docx-review-darwin-arm64"
+      sha256 "a84bb1888f61e0f7e6947033e391e7fb78c8c21aadf1a0599bf18cb7978db84a"
     else
-      url "https://github.com/henrybloomingdale/docx-review/releases/download/v1.0.0/docx-review-darwin-amd64"
-      sha256 "9279c6fc56824f45894f93704659381913017a7cc81798a4c7b317732f161ea2"
+      url "https://github.com/henrybloomingdale/docx-review/releases/download/v1.1.0/docx-review-darwin-amd64"
+      sha256 "42df83e0f323ea0bc1303d76d7a8d15c0ae668f5b1eb837eca5bd6e3a10e7203"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/henrybloomingdale/docx-review/releases/download/v1.0.0/docx-review-linux-arm64"
-      sha256 "0d971283f31f1d4f340171a9cba7682f52f753a09636a0cb311e2819ebb0bfe8"
+      url "https://github.com/henrybloomingdale/docx-review/releases/download/v1.1.0/docx-review-linux-arm64"
+      sha256 "0875dfd1aa7e8047e162c73136d19d53a8ac2110199fe41f0cd49d105abc56a1"
     else
-      url "https://github.com/henrybloomingdale/docx-review/releases/download/v1.0.0/docx-review-linux-amd64"
-      sha256 "3441954660a25eec41f37d7549810b841e4c64714462fe0dc8e6c530db7a09f9"
+      url "https://github.com/henrybloomingdale/docx-review/releases/download/v1.1.0/docx-review-linux-amd64"
+      sha256 "4a3d497da7bc0fb8c2a8a92278db695fe8de178716dcc998f796e3596abbef76"
     end
   end
 
@@ -30,6 +30,6 @@ class DocxReview < Formula
   end
 
   test do
-    assert_match "docx-review 1.0.0", shell_output("#{bin}/docx-review --version")
+    assert_match "docx-review 1.1.0", shell_output("#{bin}/docx-review --version")
   end
 end
