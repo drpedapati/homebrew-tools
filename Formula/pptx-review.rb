ed75bb9a776cb87425282acae5d4e4fa803602ae
henry <henrybloomingdale@macmini-2.local>
2026-02-14 14:48:46 -0500

    Update pptx-review to 1.0.1 (fix infinite loop bug)

--- Formula/pptx-review.rb
+++ Formula/pptx-review.rb
@@ -2,25 +2,25 @@ class PptxReview < Formula
   desc "Add comments and edit PowerPoint presentations from the CLI"
   homepage "https://github.com/henrybloomingdale/pptx-review"
   license "MIT"
-  version "1.0.0"
+  version "1.0.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/henrybloomingdale/pptx-review/releases/download/v1.0.0/pptx-review-darwin-arm64"
-      sha256 "c10b02f596d8f8538c8b1306ae02a96b901a1d277df1d95ea32ce9dce389cd22"
+      url "https://github.com/henrybloomingdale/pptx-review/releases/download/v1.0.1/pptx-review-darwin-arm64"
+      sha256 "efd98ecc87995f7fcd7c196c63f47c5e28579a044269f99f0d61976defe3a8d5"
     else
-      url "https://github.com/henrybloomingdale/pptx-review/releases/download/v1.0.0/pptx-review-darwin-amd64"
-      sha256 "87ed92d352af7d36acd09ff13e5ffc426b151a6383a9229d76f89abe6a92d457"
+      url "https://github.com/henrybloomingdale/pptx-review/releases/download/v1.0.1/pptx-review-darwin-amd64"
+      sha256 "ba2d2da7dcbc86cf2c727ed6fecc3e5986a1a5d7c5c81296ab301a3403950644"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/henrybloomingdale/pptx-review/releases/download/v1.0.0/pptx-review-linux-arm64"
-      sha256 "48e16d3171610d2bf1a3601c1225e5bf7093ea239261da6b5b8bdb2462020066"
+      url "https://github.com/henrybloomingdale/pptx-review/releases/download/v1.0.1/pptx-review-linux-arm64"
+      sha256 "da3679c1f6fdac40a8f39c6992bce1b12dbcf598c146d8cedcc472b7fe4efcb9"
     else
-      url "https://github.com/henrybloomingdale/pptx-review/releases/download/v1.0.0/pptx-review-linux-amd64"
-      sha256 "be5256494df945e5a0eae634948b1c6e89a194a3f762d019593a8434c95edcb9"
+      url "https://github.com/henrybloomingdale/pptx-review/releases/download/v1.0.1/pptx-review-linux-amd64"
+      sha256 "274749143a5e88dc127a88d40160eac9c699c7281b476448a3be37368323fc22"
     end
   end
 
@@ -30,6 +30,6 @@ class PptxReview < Formula
   end
 
   test do
-    assert_match "pptx-review 1.0.0", shell_output("#{bin}/pptx-review --version")
+    assert_match "pptx-review 1.0.1", shell_output("#{bin}/pptx-review --version")
   end
 end
