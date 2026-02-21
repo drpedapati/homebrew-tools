e378028a7aec0da9eb2e12f014e0aa45e6207fbb
Ernie Pedapati <ernest.pedapati@cchmc.org>
2026-02-21 08:42:09 -0500

    Update docx-review to v1.3.1

--- Formula/docx-review.rb
+++ Formula/docx-review.rb
@@ -2,25 +2,25 @@ class DocxReview < Formula
   desc "Read, edit, and diff Word documents with tracked changes from the CLI"
   homepage "https://github.com/drpedapati/docx-review"
   license "MIT"
-  version "1.3.0"
+  version "1.3.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/drpedapati/docx-review/releases/download/v1.3.0/docx-review-darwin-arm64"
-      sha256 "6af82faa29f04a0a4f8b4e2f3350fa47f00767d51774fd72fbf5080e888e40c3"
+      url "https://github.com/drpedapati/docx-review/releases/download/v1.3.1/docx-review-darwin-arm64"
+      sha256 "612669fde6034106ededdc3ca8085b95193720fc9a2c6837dfa8f23782764ab8"
     else
-      url "https://github.com/drpedapati/docx-review/releases/download/v1.3.0/docx-review-darwin-amd64"
-      sha256 "4a26e20fbfc30582c112bf1381303323807b47f085a497b29a76e07665b52741"
+      url "https://github.com/drpedapati/docx-review/releases/download/v1.3.1/docx-review-darwin-amd64"
+      sha256 "a7d990a88483101053e7b774b741b481b73d41afa174de51f5fcbd198b93bed8"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/drpedapati/docx-review/releases/download/v1.3.0/docx-review-linux-arm64"
-      sha256 "f18af391b2911bdcbe18ecd5a9dbf970d23ff4efd8d9bb0c1fdab8fcdbc96463"
+      url "https://github.com/drpedapati/docx-review/releases/download/v1.3.1/docx-review-linux-arm64"
+      sha256 "ed02f7e670b66d2d2396fa8da2409a3a3e2266197cd199f5a3c4f0bf9ffc5cda"
     else
-      url "https://github.com/drpedapati/docx-review/releases/download/v1.3.0/docx-review-linux-amd64"
-      sha256 "1bfb0330eb132b8e0c32eb0ff79a356dcaa2a5285d4bf2b09771dafd589bfbc0"
+      url "https://github.com/drpedapati/docx-review/releases/download/v1.3.1/docx-review-linux-amd64"
+      sha256 "b712f9da44ac0631826d580103078a3bb26900f357c49e190c3d6f549090c55f"
     end
   end
 
@@ -30,6 +30,6 @@ class DocxReview < Formula
   end
 
   test do
-    assert_match "docx-review 1.3.0", shell_output("#{bin}/docx-review --version")
+    assert_match "docx-review 1.3.1", shell_output("#{bin}/docx-review --version")
   end
 end
