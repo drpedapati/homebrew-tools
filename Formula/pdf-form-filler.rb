4d0e378e398bb1f2644599e8fc24584d9af5c504
Ernie Pedapati <ernest.pedapati@cchmc.org>
2026-05-03 07:58:49 -0400

    Bump pdf-form-filler to 0.2.1

--- Formula/pdf-form-filler.rb
+++ Formula/pdf-form-filler.rb
@@ -1,16 +1,16 @@
 class PdfFormFiller < Formula
   desc "Inspect, schema-export, fill, and flatten true AcroForm PDFs"
   homepage "https://github.com/drpedapati/csharp-pdf-filler"
-  version "0.1.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/drpedapati/csharp-pdf-filler/releases/download/v0.1.0/pdf-form-filler-v0.1.0-osx-arm64.tar.gz"
-      sha256 "2a91219864f0d3c657d345f37f2c87d17e99bc4b84946afbcfb9147dbcf7d4c4"
+      url "https://github.com/drpedapati/csharp-pdf-filler/releases/download/v0.2.1/pdf-form-filler-v0.2.1-osx-arm64.tar.gz"
+      sha256 "01113d4a24fea1c8e7e8468e36935ebc3cc493f8a4c0f8f7a0bb5c820fcc40be"
     else
-      url "https://github.com/drpedapati/csharp-pdf-filler/releases/download/v0.1.0/pdf-form-filler-v0.1.0-osx-x64.tar.gz"
-      sha256 "c75ac35a9fa9d4c8e4daf63243e7b2d82224f5fd1a913559cec2bd42f29118bc"
+      url "https://github.com/drpedapati/csharp-pdf-filler/releases/download/v0.2.1/pdf-form-filler-v0.2.1-osx-x64.tar.gz"
+      sha256 "ea3326741c547ecc5738e10db3e1018890da70fa89331c39c5d6ac9e2b4ea355"
     end
   end
 
